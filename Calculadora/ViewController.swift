5bbb8ad3d75ea42f43d3d20acf2aa25ba563ac29
Thiago Bertolino <thiagopbertolino@gmail.com>
2022-10-15 16:50:06 -0300

    Initial Commit

---
+++ Calculadora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by Thiago Bertolino on 15/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
