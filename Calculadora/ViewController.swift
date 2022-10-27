43cc954f2a0829689ca467e95375b9856c16d8ff
Thiago Bertolino <thiagopbertolino@gmail.com>
2022-10-27 16:43:53 -0300

    Up

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -9,11 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func digito(_ sender: UIButton) {
+        print("digito: \(sender.currentTitle)")
     }
-
-
+    
 }
 
